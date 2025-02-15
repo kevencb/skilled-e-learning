67194b9a33694a38d8f4fded769ddd5e4d59abd6
Kev_CB <kevcb369@gmail.com>
2025-02-15 11:53:39 -0500

    Project finished

--- js/main.js
+++ js/main.js
@@ -5,11 +5,37 @@ async function loadItems(){
         const res = await fetch('data.json')
         const items = await res.json()
         console.table(items.sections)
-        // displayItems(items.sections)
+        displayItems(items.sections)
     } catch (error) {
         console.error("No hemos encotrado la información.", error)
     }
 }
 
-
+function displayItems(items){
+    const itemsContainer = document.createElement('div')
+    main.appendChild(itemsContainer)
+    items.forEach((item, index) => {
+        const article = document.createElement('article')
+        if( index === 0){
+            article.classList.add("card__featured")
+            article.innerHTML = `
+                <h2>${item.title}</h2>
+        `
+        }
+        else{
+            article.classList.add("card__item")
+            article.innerHTML = `
+                <img src="${item.icon}" class="card__item-icon" aria-label="Animation icon" alt="Animation icon"/>
+                <div class="card__item-info">
+                    <h2>${item.title}</h2>
+                    <p>Learn the latest animation techniques to create stunning motion design and captivate your audience.</p>
+                    <a href="#" aria-label="Get Started">Get Started</a>
+                </div>
+            `
+        }
+        
+        itemsContainer.appendChild(article)
+    });
+    console.log("Hi")
+}
 loadItems()
